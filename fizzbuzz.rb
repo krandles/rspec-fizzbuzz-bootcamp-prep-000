5a4f0b2205251839a9bc451c5541f2bba57b592f
Kevin Randles <krandles@gmail.com>
2017-11-04 17:22:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(n)
   if n % 3 ==0 && n % 5 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif n % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif n % 5 == 0
-    puts "Buzz"
+    "Buzz"
   else
     nil
 end
